e6577bd7475fc8d31c36ad417d3b53125d4631fc
aravind-mohandas <aravindmohandas.2k@gmail.com>
2022-06-14 12:57:36 +0530

    Defined method to validate compositions and display them
    Composition can now be entered, and if they satisfy the basic rule of coming to beat then they are printed. The gaps are represented using dots

--- grammar/define.hs
+++ grammar/define.hs
@@ -4,30 +4,89 @@ data BeatCount = Laghu | Dhruta | Anudhruta
 
 -- | Common class to define both the Jati and Gati
 data JatiGati = Tisra | Chaturasra | Khanda | Misra | Sankirna deriving (Show, Eq)
-
 -- | Define a class for all syllables used in Konnakol
-data Syllable = Tha | Ki | Ta | Dhi | Gi | Na | Thom | Dhin | Ku | Ri | Ka | Tham | Langu | Thak |Dhim | Nam |Mi | Gdot | Gsc deriving (Show, Eq)
+data Syllable = Tha | Ki | Ta |Di | Dhi | Gi |Jho | Na | Thom |Lan|Gu| Dhin | Ku | Ri | Ka | Tham | Langu | Thak |Dhim | Nam |Mi | Gdot | Gsc deriving ( Eq)
 
--- | Define phrase as a collection of syllables
+instance Show Syllable where
+    show Gdot = "."
+    show Gsc = ";"
+    show Tha = "Tha"
+    show Ki = "Ki"
+    show Ta = "ta"
+    show Dhi = "Dhi"
+    show Gi = "Gi"
+    show Jho = "Jho"
+    show Na = "Na"
+    show Thom = "Thom"
+    show Lan = "Lan"
+    show Gu = "Gu"
+    show Dhin = "Dhin"
+    show Ku = "Ku" 
+    show Ri = "Ri" 
+    show Ka = "Ka"
+    show Tham = "Tham"  
+    show Thak ="Thak"
+    show Dhim = "Thak"
+    show Nam = "Nam"
+    show Mi = "Mi"
+    show _ = ""
+ 
 newtype Phrase = Phrase [Syllable]
 
+instance Show Phrase where
+    show (Phrase []) = ""
+    show (Phrase (x:xs)) = show x ++ show (Phrase xs)
+
+
+
 -- | Define a composition following one certain Gati using a 4 - member tuple
-newtype Composition = Composition ([(Phrase, Int)], JatiGati, Thala, JatiGati)
+newtype Composition = Composition ([([Syllable], Int)], JatiGati, Thala, JatiGati)
 
-getStringComp::Composition->String
+-- | Get the Counts per beat in a certain thala in a particular gati
+getCountPerBeat::JatiGati->Int->Int
+getCountPerBeat gati maxS
+  | gati == Chaturasra = 2^(maxS - 1)
+  | maxS == 1 = 1
+  | otherwise = fromEnum gati * 2^ max (maxS-2) 0
+
+-- | Driver function for getting a virtual representation of a composition, after validation
+getStringComp::Composition->IO()
 getStringComp (Composition (k , jati, thala, gati)) =
-    let maxSpeed = maximum $ map snd k
+    let maxS = maximum $ map snd k
+        countPerBeat = getCountPerBeat gati maxS
         b = calculateCount jati thala
-        countPerAvarta = (2^(maxSpeed - 1)) * b * fromEnum gati
-        a = convToList k maxSpeed
-    in show a
-getStrinComp _ = show ""
+        countPerAvarta = countPerBeat * b 
+        a = convToList k countPerBeat gati
+        d = if mod (length a) countPerAvarta == 0 then 
+            let c = getThalaSplitPoints jati thala countPerBeat
+                in finalDisp a thala c 0
+            else "Error"
+    in putStrLn
+ d
+getStrinComp _ = []
 
-convToList :: [(Phrase, Int)] -> Int -> [String]
-convToList [(Phrase x,s)] maxs =
-   map (\(x,s)-> concatMap (\t -> if t == Langu || t== Gsc then show t ++ concat (replicate (2^(maxs - s - 1) - 1)  "-") 
-   else show t ++ concat(replicate(2^(maxs - s) - 1) "-")) x) [(x,s)]
-convToList _ maxs = [""]
+-- | Method to calculate number of beats in a Thala based on its jati
+getThalaSplitPoints :: JatiGati -> Thala  ->Int-> [Int]
+getThalaSplitPoints _ (Thala []) _ = []
+getThalaSplitPoints j (Thala (x:xs)) cPB =
+    case x of Laghu -> cPB*fromEnum j  : getThalaSplitPoints j (Thala xs) cPB
+              Dhruta -> cPB * 2 : getThalaSplitPoints j (Thala xs) cPB
+              Anudhruta ->cPB : getThalaSplitPoints j (Thala xs) cPB
+
+-- | Core function in obtaining string from Composition
+convToList :: [([Syllable], Int)] -> Int ->JatiGati-> [Syllable]
+convToList [] maxs g= []
+convToList listas maxs g =
+   concatMap (\(x,s)-> concatMap (\t -> if t == Langu then Lan:Gu:replicate (div maxs (getCountPerBeat g s)   - 2) Gdot
+   else if  t== Gsc then replicate (div maxs (getCountPerBeat g s))  Gdot
+   else t:replicate (div maxs (getCountPerBeat g s) -1) Gdot) x) listas
+
+-- | Final display of a thala in lines with proper subdivisions
+finalDisp :: [Syllable] ->Thala -> [Int] -> Int -> String
+finalDisp s (Thala thala) arr n =
+    if null s then ""
+    else let pos = mod n (length arr)
+        in show (Phrase (take (arr !! pos) s)) ++ show (thala!!pos) ++ finalDisp (drop (arr !! pos) s) (Thala thala) arr (n+1)
 
 -- instance Basic Check for composition
 
@@ -40,9 +99,9 @@ toNum Gsc = 2
 toNum _ = 1
 
 -- | Convert a phrase into its total length
-toNumP::Phrase -> Int
-toNumP (Phrase []) = 0
-toNumP (Phrase (x:xs)) = toNum x + toNumP (Phrase xs)
+toNumP::[Syllable] -> Int
+toNumP [] = 0
+toNumP ((x:xs)) = toNum x + toNumP xs
 
 -- | Thala is a combination of BeatCounts
 newtype Thala = Thala [BeatCount]
@@ -65,9 +124,9 @@ instance Enum JatiGati where
 
 -- | Define conventional notation used to represent the Thalas
 instance Show BeatCount where
-    show Laghu = "| "
-    show Dhruta = "O "
-    show Anudhruta = "U "
+    show Laghu = " | "
+    show Dhruta = " O "
+    show Anudhruta = " U "
 
 -- | Define display of Thala based on that of the BeatCount
 instance Show Thala where
